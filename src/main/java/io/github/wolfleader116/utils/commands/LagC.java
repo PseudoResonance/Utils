7c2086957f43c0f578191a89992f579c98145132
WolfLeader116 <kaio11602@gmail.com>
2015-11-05 20:36:15 -0800

    Update 1.3.7

--- src/main/java/io/github/wolfleader116/utils/commands/LagC.java
+++ src/main/java/io/github/wolfleader116/utils/commands/LagC.java
@@ -19,9 +19,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class LagC implements CommandExecutor {
-
+	
 	private static final Logger log = Logger.getLogger("Minecraft");
-
+	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("lag")) {
 			if (sender instanceof Player) {
@@ -34,13 +34,14 @@ public class LagC implements CommandExecutor {
 					Long now = System.currentTimeMillis() - 79200000;
 					Long difference = now - Utils.plugin.startup;
 					Date datedifference = new Date(difference);
-					String uptime = String.valueOf(sdfshorthh.format(datedifference)) + "Hours" + String.valueOf(sdfshortmm.format(datedifference)) + "Minutes" + String.valueOf(sdfshortss.format(datedifference)) + "Seconds";
-					sender.sendMessage(ChatColor.AQUA + "Server Uptime: " + ChatColor.RED +  uptime);
-					sender.sendMessage(ChatColor.AQUA + "TPS: " + ChatColor.RED + TPS.getTPS() + ChatColor.AQUA + " TPS Percentage: " +  ChatColor.RED + String.valueOf(Math.round((1.0D - TPS.getTPS() / 20.0D) * 100.0D)));
-					sender.sendMessage(ChatColor.AQUA + "Maximum Memory: " + ChatColor.RED +  Runtime.getRuntime().maxMemory());
-					sender.sendMessage(ChatColor.AQUA + "Free Memory: " + ChatColor.RED +  Runtime.getRuntime().freeMemory());
-					sender.sendMessage(ChatColor.AQUA + "Total Memory: " + ChatColor.RED +  Runtime.getRuntime().totalMemory());
-					sender.sendMessage(ChatColor.AQUA + "Available Processors: " + ChatColor.RED +  Runtime.getRuntime().availableProcessors());
+					String uptime = String.valueOf(sdfshorthh.format(datedifference)) + " Hours " + String.valueOf(sdfshortmm.format(datedifference)) + " Minutes " + String.valueOf(sdfshortss.format(datedifference)) + " Seconds";
+					sender.sendMessage(ChatColor.AQUA + "Server Uptime: " + ChatColor.RED + uptime);
+					sender.sendMessage(ChatColor.AQUA + "TPS: " + ChatColor.RED + TPS.getTPS());
+					sender.sendMessage(ChatColor.AQUA + "Maximum Memory: " + ChatColor.RED + bytes(Runtime.getRuntime().maxMemory(), true));
+					sender.sendMessage(ChatColor.AQUA + "Free Memory: " + ChatColor.RED + bytes(Runtime.getRuntime().freeMemory(), true));
+					sender.sendMessage(ChatColor.AQUA + "Total Memory: " + ChatColor.RED + bytes(Runtime.getRuntime().totalMemory(), true));
+					sender.sendMessage(ChatColor.AQUA + "Available Cores: " + ChatColor.RED + Runtime.getRuntime().availableProcessors());
+					sender.sendMessage(ChatColor.AQUA + "Worlds:");
 					List<World> worlds = Bukkit.getServer().getWorlds();
 					for (World w : worlds) {
 						String worldType = "World";
@@ -63,13 +64,11 @@ public class LagC implements CommandExecutor {
 							for (Chunk chunk : w.getLoadedChunks()) {
 								tileEntities += chunk.getTileEntities().length;
 							}
-						}
-						catch (ClassCastException ex) {
+						} catch (ClassCastException ex) {
 							log.severe("World " + w + " seems to be corrupted!");
 						}
-						sender.sendMessage(ChatColor.AQUA + w.getName() + ": Type: " + ChatColor.RED  + worldType + ChatColor.AQUA + " Loaded Chunks: " + ChatColor.RED  + w.getLoadedChunks().length + ChatColor.AQUA  + " Entities: " + ChatColor.RED  + w.getEntities().size() + ChatColor.AQUA  + " Tile Entities: " + ChatColor.RED + tileEntities);
+						sender.sendMessage(ChatColor.AQUA + w.getName() + ": Type: " + ChatColor.RED + worldType + ChatColor.AQUA + " Loaded Chunks: " + ChatColor.RED + w.getLoadedChunks().length + ChatColor.AQUA + " Entities: " + ChatColor.RED + w.getEntities().size() + ChatColor.AQUA + " Tile Entities: " + ChatColor.RED + tileEntities);
 					}
-					sender.sendMessage(ChatColor.AQUA + "Worlds:");
 				} else {
 					Errors.sendError(Errors.NO_PERMISSION, p, "Utils");
 				}
@@ -80,7 +79,7 @@ public class LagC implements CommandExecutor {
 				Long now = System.currentTimeMillis() - 79200000;
 				Long difference = now - Utils.plugin.startup;
 				Date datedifference = new Date(difference);
-				String uptime = String.valueOf(sdfshorthh.format(datedifference)) + "Hours" + String.valueOf(sdfshortmm.format(datedifference)) + "Minutes" + String.valueOf(sdfshortss.format(datedifference)) + "Seconds";
+				String uptime = String.valueOf(sdfshorthh.format(datedifference)) + " Hours " + String.valueOf(sdfshortmm.format(datedifference)) + " Minutes " + String.valueOf(sdfshortss.format(datedifference)) + " Seconds";
 				List<World> worlds = Bukkit.getServer().getWorlds();
 				for (World w : worlds) {
 					String worldType = "World";
@@ -103,23 +102,31 @@ public class LagC implements CommandExecutor {
 						for (Chunk chunk : w.getLoadedChunks()) {
 							tileEntities += chunk.getTileEntities().length;
 						}
-					}
-					catch (ClassCastException ex) {
+					} catch (ClassCastException ex) {
 						log.severe("World " + w + " seems to be corrupted!");
 					}
 					log.info(w.getName() + ": Type: " + worldType + " Loaded Chunks: " + w.getLoadedChunks().length + " Entities: " + w.getEntities().size() + " Tile Entities: " + tileEntities);
 				}
 				log.info("Worlds:");
-				log.info("Available Processors: " +  Runtime.getRuntime().availableProcessors());
-				log.info("Total Memory: " +  Runtime.getRuntime().totalMemory());
-				log.info("Free Memory: " +  Runtime.getRuntime().freeMemory());
-				log.info("Maximum Memory: " +  Runtime.getRuntime().maxMemory());
-				log.info("TPS: " + TPS.getTPS() + " TPS Percentage: " + String.valueOf(Math.round((1.0D - TPS.getTPS() / 20.0D) * 100.0D)));
-				log.info("Server Uptime: " +  uptime);
+				log.info("Available Cores: " + Runtime.getRuntime().availableProcessors());
+				log.info("Total Memory: " + bytes(Runtime.getRuntime().totalMemory(), true));
+				log.info("Free Memory: " + bytes(Runtime.getRuntime().freeMemory(), true));
+				log.info("Maximum Memory: " + bytes(Runtime.getRuntime().maxMemory(), true));
+				log.info("TPS: " + TPS.getTPS());
+				log.info("Server Uptime: " + uptime);
 				log.info("===---Utils Lag Report---===");
 			}
 		}
 		return false;
 	}
-
+	
+	public static String bytes(long bytes, boolean si) {
+		int unit = si ? 1000 : 1024;
+		if (bytes < unit)
+			return bytes + " B";
+		int exp = (int) (Math.log(bytes) / Math.log(unit));
+		String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+		return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+	}
+	
 }
